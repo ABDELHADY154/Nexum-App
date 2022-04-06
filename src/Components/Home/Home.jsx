c272090107c9c32a503c132b0f4a0ba70891bbad
Abdelhady154 <mohamedabdelhady88@gmail.com>
2022-04-06 17:47:15 +0200

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { Header } from "react-native-elements";
-import { Text, Button, Input } from "react-native-elements";
+import { Text, Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/Fontisto";
 import IconAnt from "react-native-vector-icons/AntDesign";
 import IconMat from "react-native-vector-icons/MaterialCommunityIcons";
+import { Input } from "galio-framework";
+import { ListItem, Avatar } from "@rneui/themed";
 
 export default class Home extends Component {
-  state = {};
+  state = {
+    loading: false,
+  };
 
   render() {
     return (
@@ -128,7 +132,171 @@ export default class Home extends Component {
             marginVertical: "5%",
           }}
         />
-        <View></View>
+
+        <Input
+          style={{ width: "90%", alignSelf: "center" }}
+          placeholder="Search"
+          type="default"
+        />
+        <Button
+          containerStyle={{ margin: 5, width: "90%", alignSelf: "center" }}
+          loadingProps={{ animating: true }}
+          title="Search"
+          loading={this.state.loading}
+          titleStyle={{ marginHorizontal: 5 }}
+        />
+        <View style={{ width: "100%", height: "42%" }}>
+          <ScrollView style={{ width: "100%", height: "35%" }}>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+            <ListItem friction={90} tension={100} activeScale={0.95}>
+              <Avatar
+                rounded
+                source={{
+                  uri: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+                }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  Chris Jackson
+                </ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+          </ScrollView>
+
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              backgroundColor: "#2089DC",
+              height: "30%",
+            }}
+          >
+            <Button
+              buttonStyle={{
+                width: 50,
+                height: 50,
+                borderRadius: 1000,
+                backgroundColor: "#fff",
+                marginLeft: "25%",
+              }}
+              containerStyle={{ margin: 5, borderRadius: 1000 }}
+              icon={<IconAnt name="user" size={25} color="#2089DC" />}
+              iconContainerStyle={{ background: "#2089DC" }}
+              loadingProps={{ animating: true }}
+              loadingStyle={{}}
+              onPress={() => alert("click")}
+              titleProps={{}}
+            />
+            <Text
+              style={{
+                color: "#fff",
+                alignSelf: "flex-start",
+                marginLeft: "4%",
+              }}
+            >
+              Refresh Contacts
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
